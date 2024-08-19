1a72066d57c7f59654b91a8e68ad55e6dbfdcf04
Uros <uros.bo97@gmail.com>
2024-08-19 22:03:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x=3, y=2;
+        int z = x+y;
+
+
+        System.out.printf("Rezultat je:" +z);
+    }
+}
