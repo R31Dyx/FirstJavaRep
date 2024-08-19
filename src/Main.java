0fa1ef6d075c00ace6910793eaf2be920d27b180
Uros <uros.bo97@gmail.com>
2024-08-20 00:00:04 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int x=3, y=2;
-        int z = x+y;
+        int x=3, y=2, z=6;
+        if (x>y){
+            System.out.println("Veci je X: " +x);
+        }else {
+            x=y;
+            System.out.println("Y je veci od X:" +x);
+        }
+        if(y>z){
+            System.out.println("Y je veci od Z");
+
+        }
 
 
-        System.out.printf("Rezultat je:" +z);
+        int t = x+y;
+
+
+        System.out.printf("Rezultat je:" +t);
     }
 }
